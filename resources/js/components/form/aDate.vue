e68a5a693762aa6267e7022749f0b99d0af78393
MarceloPreis <marcelo.s.preis@gmail.com>
2024-10-13 12:54:01 +0000

    iniciado componente de data

---
+++ resources/js/components/form/aDate.vue
@@ -0,0 +1,42 @@
+<template>
+    <Field v-bind="$props" v-model:focused="focused">
+          <VueDatePicker v-model="date"></VueDatePicker>
+    </Field>
+</template>
+
+<script>
+import Field from '@/components/form/Field.vue'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+    name: 'aInput',
+    components: { Field, VueDatePicker },
+    emits: ['update:model-value'],
+
+    data() {
+        return {
+            focused: false,
+        }
+    },
+
+    props: {
+        modelValue: {
+            type: [String, Number]
+        },
+        type: String,
+        label: String,
+    },
+
+    computed: {
+        showValue: {
+            get() {
+                return this.modelValue
+            },
+            set(value) {
+                return this.$emit('update:model-value', value)
+            }
+        }
+    }
+}
+</script>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -5,15 +5,17 @@
     <br />
     
     <aPhone label="aPhone"></aPhone>
+    <aDate label="aDate"></aDate>
 </template>
 
 <script>
 import aInput from '../components/form/aInput.vue'
 import aPhone from '../components/form/aPhone.vue'
+import aDate from '../components/form/aDate.vue'
 
 
 export default {
-    components: { aInput, aPhone },
+    components: { aInput, aPhone, aDate },
 
     data() {
         return {
